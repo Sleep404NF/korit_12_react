f62627353281aa35609d8cc3073f319eb278ec03
rgn2698-wq <rgn2698@gmail.com>
2026-03-10 12:40:29 +0900

    02060310

---
+++ gitapi/src/App.jsx
@@ -0,0 +1,25 @@
+import {useQuery} from '@tanstack/react-query'
+import './App.css'
+import {fetchPosts} from './api'
+
+
+function App() {
+
+  const {isLoading, data: posts } = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchPosts
+  });
+
+  
+  return (
+    <>
+      {
+        isLoading ?
+          '로딩중...'
+          : posts.map((post, index) => <div>{`${index} : ${post.title}` }</div>)
+      }
+    </>
+  )
+}
+
+export default App
